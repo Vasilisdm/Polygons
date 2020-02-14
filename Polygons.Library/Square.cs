84581234eca2795390902cf0d2e874dc447c8a47
vasilisdimitriou <vasilisdmr@gmail.com>
2020-02-14 23:45:50 +0200

    Implemented square class which inherits from ConcreteRegularPolygon.

---
+++ Polygons.Library/Square.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Polygons.Library
+{
+    public class Square : ConcreteRegularPolygon
+    {
+        public Square(int length) : base(4, length)
+        {
+        }
+
+        public override double GetArea()
+        {
+            return SideLength * SideLength;
+        }
+    }
+}
