ab538334a6fb7cf70d82a6d8d250629d34341bb9
vasilisdimitriou <vasilisdmr@gmail.com>
2020-02-14 23:59:25 +0200

    Added octagon class which implements IRegularPolygon interface.

---
+++ Polygons.Library/Octagon.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Polygons.Library
+{
+    public class Octagon : IRegularPolygon
+    {
+        public Octagon()
+        {
+        }
+
+        public int NumberOfSides { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public int SideLength { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public double GetArea()
+        {
+            throw new NotImplementedException();
+        }
+
+        public double GetPerimeter()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
