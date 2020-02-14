5c81b813048f61a22e9a081e92ea34635b5e5239
vasilisdimitriou <vasilisdmr@gmail.com>
2020-02-14 23:35:28 +0200

    Added polygons library project.

---
+++ Polygons.Library/ConcreteRegularPolygon.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Polygons.Library
+{
+    public class ConcreteRegularPolygon
+    {
+        public int NumberOfSides { get; set; }
+        private int _sideLength;
+        public int SideLength { get { return _sideLength; } set { _sideLength = value; } };
+    }
+}
