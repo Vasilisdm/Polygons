1284f78f75642356593fb83257f96bc0b88725fe
vasilisdimitriou <vasilisdmr@gmail.com>
2020-02-14 23:38:28 +0200

    Added properties, GetPerimeter and GetArea methods.

--- Polygons.Library/ConcreteRegularPolygon.cs
+++ Polygons.Library/ConcreteRegularPolygon.cs
@@ -5,7 +5,22 @@ namespace Polygons.Library
     public class ConcreteRegularPolygon
     {
         public int NumberOfSides { get; set; }
-        private int _sideLength;
-        public int SideLength { get { return _sideLength; } set { _sideLength = value; } };
+        public int SideLength { get; set; }
+
+        public ConcreteRegularPolygon(int sides, int length)
+        {
+            NumberOfSides = sides;
+            SideLength = length;
+        }
+
+        public double GetPerimeter()
+        {
+            return NumberOfSides * SideLength;
+        }
+
+        public virtual double GetArea()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
